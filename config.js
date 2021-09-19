127d6f2bf8df6f73fc8ae9e1a4942b7f37686ecf
crawdy2k2 <91015704+crawdy2k2@users.noreply.github.com>
2021-09-20 00:56:01 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,19 @@
 const CONFIG = {
-    introTitle: 'Hii ChiChi!',
+    introTitle: 'Babe à!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao Chi cứ đi nhầm đường
-    Lạc vào tim tớ lẻ loi`,
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Trêu Chi tí thôi à 😜',
-    desc: 'Chi có muốn nói chuyện với pa chút ko ',
-    btnYes: 'cóooooo',
-    btnNo: 'Không Pa à :3',
-    mess: 'Tớ biết mà 😗',
-    messDesc: 'Mình bắt đầu trò chuyện nhé!',
-    btnAccept: 'Okiii <3',
-    messLink: 'https://www.facebook.com/messages/t/100026031100253' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là thích anh <33',
+    btnNo: 'Không, Anh trai à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Tối nay anh qua đón nhé công chúa.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/messages/t/100026031100253 //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
